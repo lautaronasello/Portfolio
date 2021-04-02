85a5dbd905e02dd47b1e93f4c158c2e4398f088c
Lautaro Nasello <lautaronasello@gmail.com>
2021-04-01 21:41:59 -0300

    add portfolio to the web

--- src/App.js
+++ src/App.js
@@ -7,18 +7,15 @@ import Knowledge from "./knowledge";
 import Experience from "./experience";
 import Contact from "./contact";
 
-/* import Projects from "./projects";
- */
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/knowledge" component={Knowledge} />
-          <Route exact path="/education" component={Experience} />
-          {/* <Route exact path="/projects" component={Projects} /> */}
+          <Route exact path="/portfolio" component={LandingPage} />
+          <Route exact path="/portfolio/about" component={About} />
+          <Route exact path="/portfolio/knowledge" component={Knowledge} />
+          <Route exact path="/portfolio/education" component={Experience} />
 
           <Route exact path="/contact" component={Contact} />
         </Switch>
