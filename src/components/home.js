bf6305c2340b81a872e01e1f295efc8ddb4f2f42
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-12 18:08:24 -0300

    new card add

--- src/components/home.js
+++ src/components/home.js
@@ -1,73 +1,87 @@
 import React, { Fragment } from 'react';
 import Title from './title';
 import Card from './card';
-import { Link } from 'react-router-dom';
 import birreteimg from './img/birrete.png';
 import linkedinimg from './img/linkedin.png';
 import brainimg from './img/brain.png';
 import userimg from './img/user1.png';
-
+import projectsimg from './img/projects.png';
+import { Col, Container, Row } from 'react-bootstrap';
 class Home extends React.Component {
   render() {
+    const style = {
+      width: '14rem',
+      height: '17.5rem',
+    };
+
     return (
       <Fragment>
-        <div className='text-center pt-5 mt-1 position-static'>
-          <Title title='Lautaro Nasello' subtitle='Frontend Developer Web' />
-
-          <div className='card-group text-center mt-5 mx-0'>
-            <div className='container'>
-              <div className='row'>
-                <div className='col-sm-3 col-md-3 col-lg-3'>
-                  <div className='mx-1'>
-                    <Link to='/about'>
-                      <Card
-                        img={userimg}
-                        section='About'
-                        text='Something about my personal life'
-                      />
-                    </Link>
-                  </div>
-                </div>
-                <div className='col-sm-3 col-md-3 col-lg-3'>
-                  <div className='mx-1'>
-                    <Link to='/Knowledge'>
-                      <Card
-                        img={brainimg}
-                        section='Knowledge'
-                        text='My knowledge from new tecnologies'
-                      />
-                    </Link>
-                  </div>
-                </div>
-                <div className='col-sm-3 col-md-3 col-lg-3'>
-                  <div className='mx-1'>
-                    <Link to='/education'>
-                      <Card
-                        img={birreteimg}
-                        section='Education'
-                        text='My experience and learnig achademys'
-                      />
-                    </Link>
-                  </div>
-                </div>
-                <div className='col-sm-3 col-md-3 col-lg-3'>
-                  <div className='mx-1'>
-                    <Link to='/contact'>
-                      <Card
-                        img={linkedinimg}
-                        section='Contact'
-                        text='Social Media to contact and chat me!'
-                      />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Container className='mx-auto px-0' style={{ marginTop: '5vh' }}>
+          <Row>
+            <Col md={12} lg={12}>
+              <Title
+                title='Lautaro Nasello'
+                subtitle='Frontend Developer Web'
+              />{' '}
+            </Col>
+          </Row>
+          <Row className=''>
+            {' '}
+            <Col sm={2} lg={2} className='m-auto'>
+              <Card
+                img={userimg}
+                section='About'
+                text='Something about my personal life'
+                btn='about'
+                style={style}
+                href='/about'
+              />
+            </Col>
+            <Col sm={2} lg={2} className='m-auto'>
+              <Card
+                img={brainimg}
+                section='Knowledge'
+                text='Something about my personal life'
+                btn='Knowledge'
+                style={style}
+                href='/knowledge'
+              />
+            </Col>
+            <Col sm={2} lg={2} className='m-auto'>
+              <Card
+                img={birreteimg}
+                section='Education'
+                text='Something about my personal life'
+                btn='Education'
+                style={style}
+                href='/education'
+              />
+            </Col>
+            <Col sm={2} lg={2} className='m-auto'>
+              <Card
+                img={projectsimg}
+                section='Projects'
+                text='Projects for training and learning'
+                btn='Projects'
+                style={style}
+                href='/education'
+              />
+            </Col>
+            <Col sm={2} lg={2} className='m-auto'>
+              <Card
+                img={linkedinimg}
+                section='Contact'
+                text='Something about my personal life'
+                btn='Contact'
+                style={style}
+                href='/contact'
+              />
+            </Col>
+          </Row>
+        </Container>
+        {/*  */}
       </Fragment>
     );
   }
 }
-
 export default Home;
