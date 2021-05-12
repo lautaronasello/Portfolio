392fef452b90f415c9d37293993e842eefc8fb0a
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-12 18:07:26 -0300

    erase landing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
-import LandingPage from "./landingPage";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Layout from "./components/layout";
-import About from "./About";
-import Knowledge from "./knowledge";
-import Experience from "./experience";
-import Contact from "./contact";
+import './App.css';
+import Home from './components/home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Layout from './components/layout';
+import About from './About';
+import Knowledge from './knowledge';
+import Experience from './experience';
+import Contact from './contact';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/knowledge" component={Knowledge} />
-          <Route exact path="/education" component={Experience} />
-          <Route exact path="/contact" component={Contact} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/knowledge' component={Knowledge} />
+          <Route exact path='/education' component={Experience} />
+          <Route exact path='/contact' component={Contact} />
         </Switch>
       </Layout>
     </BrowserRouter>
